de124c1288c7b47050961eedfe1c1d994b7f29ac
Fazo01 <fg7829098@gmail.com>
2025-07-02 13:04:13 +0500

    Hero Slider

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,37 @@
-import React from 'react'
+import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+
+import hero1 from '../assets/hero-1.png';
+import hero2 from '../assets/hero-2.png';
+import hero3 from '../assets/hero-3.png';
+import {Autoplay, Parallax} from 'swiper/modules'
+const slides = [hero1, hero2, hero3];
 
 const Header = () => {
   return (
-    <div>
-      
+    <div className='min-h-screen w-full relative'>
+      <Swiper className='h-full w-full' loop={true} slidesPreView={1} spaceBetween={0} autoplay={{delay:2500,}} speed={1500} parallax={true} modules={[Autoplay,Parallax]}>
+        {slides.map((image, index) => (
+          <SwiperSlide key={index}>
+            <div
+              className="h-screen w-full bg-no-repeat bg-cover bg-center relative gradient-after"
+              style={{ backgroundImage: `url(${image})` }}
+            >
+              <div className="absolute bottom-[18%] left-[15%] z-2 flex flex-col gap-[18px] ">
+                <small className='text-white text-[22px]' data-swiper-parallax="-200">Luxury Hotel & Restaurant</small>
+                <h2 data-swiper-parallax="-300" className='text-white text-[46px]'>
+                  Enjoy Your <span className='text-[#df8033]'>Dream</span> Time with <br />
+                  <span className='text-[#df8033]'>Luxury</span> Experience
+                </h2>
+                <p data-swiper-parallax="-400" className='text-white text-[20px] font-poppins'>Call Now <span className='text-[#df8033]  block font-semibold'>9876543210</span></p>
+              </div>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,23 +4,24 @@ const Nav = () => {
   return (
     <div className="h-[80px] w-full flex justify-evenly items-center fixed z-99 top-0 bg-transparent">
       <div >
-        <a href="" className='text-[25px] font-bold text-[#fff]'><span className='text-[#4f3520]'>ROYALX</span></a>
+        <a href="" className='text-[25px] font-bold text-[#fff]'><span className='text-[#df8033]'>ROYAL</span>X</a>
       </div>
       <ul className=''>
         {navlink.map((data,index)=>(
             <li key={data.id} className={`inline-block ${index===navlink.length-1?"pr-0":"pr-[15px]"}`}>
-                <a className='text-[#1e1e1e] font-medium font-poppins text-[15px]' href={`${data.id}`}>{data.title}</a>
+                <a className='text-[#fff] font-medium font-poppins text-[15px]' href={`${data.id}`}>{data.title}</a>
             </li>
         ))}
       </ul>
       <div className="flex gap-[18px] items-center">
-<button className="cursor-pointer h-[45px] w-[150px] border-2 border-white hover:border-black bg-transparent hover:bg-black text-black hover:text-white text-[15px] font-medium transition-colors duration-300">
+        <button className="cursor-pointer h-[38px] w-[130px] border-none bg-[#cb5c00] tracking-[2px] text-white rounded-[30px] text-[15px] font-medium shadow-2px hover:shadow-h2px transform scale-100 hover:scale-[0.95] transition duration-1000">
   Book Now
 </button>
 
 
 
-        <i className='ri-menu-4-line'></i>
+
+        <i className='sm:hidden  ri-menu-4-line border-none bg-[#cb5c00] h-[40px] w-[40px] flex justify-center items-center font-[15px] shadow-2px hover:shadow-h2px text-[15px] transition duration-1000 transform scale-100 hover:scale-[0.95] cursor-pointer' ></i>
       </div>
     </div>
   )
